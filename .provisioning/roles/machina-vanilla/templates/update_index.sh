101961ab632e84f84a3a624ef7dc16cd83aa1de7
Morgan Aubert <morgan.aubert@zoho.com>
2017-09-26 21:05:04 -1000

    Removed provisioning folder

--- .provisioning/roles/machina-vanilla/templates/update_index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activates virtualenv
-. {{ django_env_path }}/bin/activate
-export DJANGO_INSTALL_PATH={{ django_path }}
-cd {{ django_project_path }}
-
-# Updates the Whoosh index
-python manage.py update_index
